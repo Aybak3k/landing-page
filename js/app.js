92dca74ece4621bd2c1ae05824a6d8c3577a2cb3
NourAybak <nooreldin3000@gmail.com>
2021-03-18 18:24:07 +0200

    almost done

--- js/app.js
+++ js/app.js
@@ -1,35 +1,29 @@
 // Global Vars
-const sections = document.querySelectorAll('section');
-
+const sections = document.querySelectorAll("section");
 
 // Helper Funcs
-const lg = val => console.log(val);
+const isInViewport = () => {
+    for (const section of sections){
+        if (section.getBoundingClientRect().top < window.innerHeight) {
+            section.classList.add('your-active-class')
+        }
+        if (section.getBoundingClientRect().top < -255) {
+            section.classList.remove('your-active-class')
+        }
+    }
+}
+
 
 // build the nav
-const navbarList = document.querySelector('#navbar__list');
+const navbarList = document.querySelector("#navbar__list");
 let frag = document.createDocumentFragment();
 for (const section of sections){
-    let li = document.createElement('li');
-    li.innerHTML = `<a href="#${section.getAttribute('id')}" class="menu__link">${section.getAttribute('data-nav')}</a>`
+    let li = document.createElement("li");
+    li.innerHTML = `<a href="#${section.getAttribute('id')}" class="menu__link">${section.getAttribute("data-nav")}</a>`
     frag.appendChild(li)
 }
 navbarList.appendChild(frag);
-// ---------------------------------------------------------------------------------
 
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
 
 // Set sections as active
+window.addEventListener("scroll", isInViewport)
