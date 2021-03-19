8b3da57e0c003cd88b34fd1baf03235b8ecaa4f3
NourAybak <nooreldin3000@gmail.com>
2021-03-19 17:02:10 +0200

    made nav responsive

--- js/app.js
+++ js/app.js
@@ -1,21 +1,30 @@
 // Global Vars
 const sections = document.querySelectorAll("section");
+const navbarList = document.querySelector("#navbar__list");
+const burger = document.querySelector(".burger")
 
 // Helper Funcs
 const isInViewport = () => {
     for (const section of sections){
         if (section.getBoundingClientRect().top < window.innerHeight) {
-            section.classList.add('your-active-class')
+            section.classList.add("your-active-class")
         }
         if (section.getBoundingClientRect().top < -255) {
-            section.classList.remove('your-active-class')
+            section.classList.remove("your-active-class")
         }
+    }
+}
+
+const showLinks = () => {
+    if (window.getComputedStyle(navbarList).display === "none") {
+        navbarList.style.display = "flex"
+    } else if (window.getComputedStyle(navbarList).display === "flex") {
+        navbarList.style.display = "none"
     }
 }
 
 
 // build the nav
-const navbarList = document.querySelector("#navbar__list");
 let frag = document.createDocumentFragment();
 for (const section of sections){
     let li = document.createElement("li");
@@ -27,3 +36,6 @@ navbarList.appendChild(frag);
 
 // Set sections as active
 window.addEventListener("scroll", isInViewport)
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+burger.addEventListener("click", showLinks)
